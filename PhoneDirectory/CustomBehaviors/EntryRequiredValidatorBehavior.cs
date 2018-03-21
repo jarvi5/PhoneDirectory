0f4e6324f45497ad8367c08b15b7195e0744b48a
Javier Camacho <javicamdo@gmail.com>
2018-03-21 16:49:11 -0400

    show contact form, save data in firebase and validate fields

---
+++ PhoneDirectory/CustomBehaviors/EntryRequiredValidatorBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace PhoneDirectory.Core.CustomBehaviors
+{
+    public class EntryRequiredValidatorBehavior : Behavior<Entry>
+    {
+        public string MessageLabel { get; set; }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            var entry = (Entry)sender;
+            bool isValid = !string.IsNullOrEmpty(entry.Text);
+            Label errorLabel = (entry).FindByName<Label>(MessageLabel);
+            errorLabel.Text = isValid ? "" : "This field is required";
+            errorLabel.IsVisible = !isValid;
+        }
+    }
+}
--- PhoneDirectory/Services/FirebaseContactsService.cs
+++ PhoneDirectory/Services/FirebaseContactsService.cs
@@ -8,18 +8,44 @@ namespace PhoneDirectory.Core.Services
 {
     public class FirebaseContactsService : IContactsService
     {
-        HttpClient HttpClient { get; set; }
+        HttpClient _httpClient;
+        string _baseUrl;
 
         public FirebaseContactsService()
         {
-            HttpClient = new HttpClient();
-            HttpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            _httpClient = new HttpClient();
+            _baseUrl = "https://phonedirectory-8eaff.firebaseio.com/";
+            _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
         }
 
         public async Task<List<Contact>> GetContacts()
         {
-            string contacts = await HttpClient.GetStringAsync("https://phonedirectory-8eaff.firebaseio.com/Contacts.json");
+            string contacts = await _httpClient.GetStringAsync(_baseUrl + "Contacts.json");
             return JsonConvert.DeserializeObject<List<Contact>>(contacts);
+        }
+
+        public async Task<bool> CreateContact(Contact contact)
+        {
+            int index = await GetIndex(contact);
+            string stringContact = JsonConvert.SerializeObject(contact);
+            StringContent content = new StringContent(stringContact);
+            HttpResponseMessage response = await _httpClient.PostAsync(_baseUrl + "Contacts.json", content);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> EditContact(Contact contact)
+        {
+            int index = await GetIndex(contact);
+            string stringContact = JsonConvert.SerializeObject(contact);
+            StringContent content = new StringContent(stringContact);
+            HttpResponseMessage response = await _httpClient.PutAsync(_baseUrl + "Contacts/"+ index +".json", content);
+            return response.IsSuccessStatusCode;
+        }
+
+        private async Task<int> GetIndex(Contact contact)
+        {
+            List<Contact> contacts = await GetContacts();
+            return contacts.FindIndex((obj) => obj.PrimaryPhone == contact.PrimaryPhone);
         }
     }
 }
